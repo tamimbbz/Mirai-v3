84d9787da75fb8d651cb434ea0a9db72105a4f23
rxabdullah0007 <rxabdullah0007@gmail.com>
2025-12-17 18:51:41 +0600

    Update imgur.js

--- Script/commands/imgur.js
+++ Script/commands/imgur.js
@@ -1,9 +1,9 @@
 module.exports.config = {
  name: "imgur",
- version: "1.0.0", 
+ version: "2.7.0", 
  hasPermssion: 0,
- credits: "Islamick Cyber Chat",
- description: "create your video link",
+ credits: "rX",
+ description: "create your imgur link",
  commandCategory: "other", 
  usages: "[tag]", 
  cooldowns: 0,
@@ -16,7 +16,7 @@ const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Sha
  const Shaon = apis.data.imgur
  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
- if(!linkanh) return api.sendMessage('╭•┄┅══❁🌺❁══┅┄•╮\n\n আসসালামু আলাইকুম-!!🖤💫\n আপনি যেই ছোবি টাকে Imgur link বানাতে চান সেই ছোবি টা imgur লিখে রিপ্লাই করুন \n\n╰•┄┅══❁🌺❁══┅┄•╯', event.threadID, event.messageID)
+ if(!linkanh) return api.sendMessage('「imgur」 What are you trying', event.threadID, event.messageID)
 const res = await axios.get(`${Shaon}/imgur?link=${encodeURIComponent(linkanh)}`); 
 var img = res.data.uploaded.image;
  return api.sendMessage(`"${img}",`, event.threadID, event.messageID);
